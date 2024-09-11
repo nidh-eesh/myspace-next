a4495559d4c91bf26e2c09ea8ce44994c18daf34
Nidheesh <nidheeshdevops@gmail.com>
2024-09-11 11:50:55 +0530

    feat: Refactor blog post page to use static generation

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -8,9 +8,20 @@ interface Props {
     params: { slug: string };
 }
 
-export default async function BlogPost({ params }: Props) {
-    const posts: Post[] = await fetch("http://localhost:3000/api/content").then((res) => res.json());
+export async function generateStaticParams() {
+    const posts: Post[] = await fetch('http://localhost:3000/api/content').then(
+        (res) => res.json()
+    );
 
+    return posts.map((post) => ({
+        slug: post.slug,
+    }));
+}
+
+export default async function BlogPostPage({ params }: Props) {
+    const posts: Post[] = await fetch('http://localhost:3000/api/content').then(
+        (res) => res.json()
+    );
     const post = posts.find((post) => post.slug === params.slug);
 
     return (

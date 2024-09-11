0e073edd65efce50fc3c3962eaa183f6832bf474
Nidheesh <nidheeshdevops@gmail.com>
2024-09-11 11:49:25 +0530

    feat: Add buttons component for sign in and sign out functionality

---
+++ src/components/buttons.tsx
@@ -0,0 +1,42 @@
+'use client';
+import { useSession, signIn, signOut } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
+
+function SignInButton() {
+    const { data: session, status } = useSession();
+  console.log(session, status);
+
+  if (status === 'loading') {
+    return <>...</>;
+  }
+
+  if (status === 'authenticated') {
+    return (
+      <Link href={`/dashboard`}>
+        <Image
+          src={session.user?.image ?? '/mememan.webp'}
+          width={32}
+          height={32}
+          alt="Your Name"
+        />
+      </Link>
+    );
+  }
+
+  return <button onClick={() => signIn()}>Sign in</button>;
+}
+
+function SignOutButton() {
+  return (
+    <button
+      onClick={() => {
+        signOut();
+      }}
+    >
+      Sign out
+    </button>
+  );
+}
+
+export { SignInButton, SignOutButton };

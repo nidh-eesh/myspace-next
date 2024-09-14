608bdbb4d36ece0375c8bafe5c70a2b85f411119
Nidheesh <nidheeshdevops@gmail.com>
2024-09-14 10:06:40 +0530

    feat: Add Prisma adapter for authentication

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,7 +1,9 @@
 import NextAuth, { type Session, type User } from "next-auth";
 import GitHub from "next-auth/providers/github";
-
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "./prisma";
 
 export const { handlers: { GET, POST }, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [GitHub]
 });
